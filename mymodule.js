25bfbeb04a314258f782a40b5b352d533103f79a
Mulky Ayyubi <170484966+MulkyAyyubi@users.noreply.github.com>
2024-09-11 14:53:26 +0700

    removed txt again

---
+++ mymodule.js
@@ -0,0 +1,22 @@
+const fs = require ("fs");
+const path = require("path");
+
+const myFunction = (dirName, ext, callback) => {
+    fs.readdir(dirName, (err,data) => {
+        if (err) {
+            callback(err);
+            return;
+        }
+
+        callback(
+            null, 
+            data.filter((name) => {
+                return "." + ext === path.extname(name);
+            })
+        );
+    });
+};
+
+module.exports = myFunction;
+
+
